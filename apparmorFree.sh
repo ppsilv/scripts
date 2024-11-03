cb5df142772e95e3b0bce3f4ad76fac3765e04f5
pdsilva <ppsilv@gmail.com>
2024-11-03 11:10:30 -0300

    updating...

--- apparmorFree.sh
+++ apparmorFree.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
-sudo systemctl stop apparmor                                                                                      
+sudo systemctl stop apparmor
 sudo systemctl disable apparmor
-
 sudo sysctl -w kernel.apparmor_restrict_unprivileged_userns=0
 
