4b9af14b34e5a40d81b886365f9ce088e3220374
pdsilva <ppsilv@gmail.com>
2025-02-10 21:06:02 -0300

    Update from latitude

--- apt-update.py
+++ apt-update.py
@@ -2,8 +2,9 @@ import syslog
 import os
 import logging
 import logging.handlers
+import subprocess
 
-version = "1.0.1"
+version = "1.0.0"
 my_logger = logging.getLogger('MyLogger')
 my_logger.setLevel(logging.DEBUG)
 handler = logging.handlers.SysLogHandler(address = '/dev/log')
@@ -14,24 +15,28 @@ pname=os.path.basename(__file__)
 my_logger.debug("Process "+pname+ " started ")
 
 def check_string():
-    with open('/tmp/aptupdate.log') as temp_f:
+    with open('/home/pdsilva/log/aptupdate.log') as temp_f:
         datafile = temp_f.readlines()
     for line in datafile:
         if 'All packages are up to date.' in line:
+            my_logger.debug(date +'%m/%d %H:%M:%S'+"Process "+pname+ " nothing to update")
             return True # The string is found
-    return False  # The string does not exist in the file "+pname+ " started ")
+        return
+            cmd = ['echo', 'I like potatos']
+            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+o, e = proc.communicate()		    data = os.system('date +'%m/%d %H:%M:%S'')
+            my_logger.debug(data+"Process "+pname+ " Updating...")
+            False # The string does not exist in the file "+pname+ " started ")
 
 
-os.system('apt update 1>/tmp/aptupdate.log 2>/dev/null')
-
+os.system('apt update 1>/home/pdsilva/log/aptupdate.log 2>/dev/null')
 
 
 if check_string():
-  my_logger.debug("System is up to date...")
-  my_logger.debug("Process "+pname+ " RUNNING OUT no more to do")
+  my_logger.debug(date +'%m/%d %H:%M:%S'+"System is up to date...")
 else:
-  my_logger.debug("Process "+pname+ " RUNNING IN we have packages do update")
-  os.system('apt -y upgrade 1>/tmp/aptupdate.log 2>/dev/null')
+  os.system('apt -y upgrade 1>/home/pdsilva/log/aptupdate.log 2>/dev/null')
 
-my_logger.debug("Process "+pname+ " finished ")
+my_logger.debug(date +'%m/%d %H:%M:%S'+"Process "+pname+ " finished ")
 
